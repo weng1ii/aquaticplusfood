6462bda2c4b7e88af81b46175d5bda9ca3d192d9
weng1ii <shandabylsasha@gmail.com>
2025-06-23 22:14:15 +0300

    first commit

---
+++ src/main/java/net/weng1i/aquaticplusfood/Aquaticplusfood.java
@@ -0,0 +1,54 @@
+package net.weng1i.aquaticplusfood;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+
+@Mod(Aquaticplusfood.MOD_ID)
+public class Aquaticplusfood
+{
+    public static final String MOD_ID = "aquaticplusfood";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public Aquaticplusfood(FMLJavaModLoadingContext context)
+    {
+
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
